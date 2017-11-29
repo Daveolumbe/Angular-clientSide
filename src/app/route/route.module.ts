bc8872adcdc734490351f62c1691ecafa9c01c67
Oluwasegun David Olumbe <david@twililightstunt.com>
2017-11-29 21:13:15 +0000

    fix styling bundle bugs

--- src/app/route/route.module.ts
+++ src/app/route/route.module.ts
@@ -1,24 +1,25 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home/home.component';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { RouterModule, Routes } from '@angular/router';
-"use strict";
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from '../home/home.component';
+import {LoginComponent} from '../login/login.component';
+import {RegisterComponent} from '../register/register.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+import {RouterModule, Routes} from '@angular/router';
+
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true })
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   exports: [RouterModule],
   declarations: []
 })
-export class RouteModule { }
+export class RouteModule {
+}
